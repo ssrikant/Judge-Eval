b7f82758fb2090a9d5d15d7e880a7aca0735077c
Malika Mission <malikamission@gmail.com>
2016-11-09 10:20:34 -0800

    Update slogout.php
    completely changed logout

--- slogout.php
+++ slogout.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-
 <?php
-  session_start();
-?>
-<html>
-
-<body>
-    <?php session_unset();
+    session_start();
+    session_unset();
     session_destroy();
-    ?>
-</body>
+    header("location:index.php"); //Replace with Logged Out page. Remove if you want to use HTML in same file.
+?>
 
+// Below is not needed, unless header above is missing. In that case, put logged out text here.
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Untitled Document</title>
+    </head>
+    <body>
+        You have logged out!
+    </body>
 </html>
